0d1c6bc39eca8f70c4eb23e6560a83330e2d1898
Joiscilene <joisci.lene@hotmail.com>
2022-03-07 21:45:47 -0300

    Adicionando correção erros

--- src/pages/Album.js
+++ src/pages/Album.js
@@ -3,7 +3,9 @@ import React from 'react';
 class Album extends React.Component {
   render() {
     return (
-      <div data-testid="page-album"></div>
+      <div
+        data-testid="page-album"
+      />
     );
   }
 }
--- src/pages/Profile.js
+++ src/pages/Profile.js
@@ -3,7 +3,9 @@ import React from 'react';
 class Profile extends React.Component {
   render() {
     return (
-      <div data-testid="page-profile"></div>
+      <div
+        data-testid="page-profile"
+      />
     );
   }
 }
