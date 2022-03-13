500402ed7c997eff0c436809e6a728a790f5f316
Joiscilene <joisci.lene@hotmail.com>
2022-03-13 02:48:14 -0300

    Adicionando requisitos 5 e 7

--- src/pages/Album.js
+++ src/pages/Album.js
@@ -1,14 +1,75 @@
 import React from 'react';
+import PropTypes from 'prop-types';
 import Header from '../componentes/Header';
+import getMusics from '../services/musicsAPI';
+import Loading from '../componentes/Loading';
+import MusicCard from '../componentes/MusicCard';
 
 class Album extends React.Component {
+  constructor() {
+    super();
+
+    this.state = {
+      musics: [],
+      loading: true,
+    };
+    this.musicsList = this.musicsList.bind(this);
+  }
+
+  componentDidMount() {
+    this.musicsList();
+  }
+
+  async musicsList() {
+    const { match } = this.props;
+    const { params } = match;
+    const { id } = params;
+
+    const musicAlbum = await getMusics(id);
+    const result = [...musicAlbum];
+    const musics = result;
+
+    this.setState({
+      musics,
+      loading: false,
+    });
+  }
+
   render() {
+    const { musics, loading } = this.state;
     return (
       <div data-testid="page-album">
         <Header />
+        { loading ? <Loading /> : (
+          <div>
+            <div>
+              <h3 data-testid="artist-name">{ musics[0].artistName }</h3>
+              <h4 data-testid="album-name">{ musics[0].collectionName }</h4>
+            </div>
+            <div>
+              {musics.map(({ trackName, previewUrl }, music) => {
+                if (music > 0) {
+                  return (<MusicCard
+                    key={ music }
+                    trackName={ trackName }
+                    previewUrl={ previewUrl }
+                  />);
+                }
+                return null;
+              })}
+            </div>
+          </div>
+        )}
       </div>
     );
   }
 }
+Album.propTypes = {
+  match: PropTypes.shape({
+    params: PropTypes.shape({
+      id: PropTypes.string.isRequired,
+    }),
+  }).isRequired,
+};
 
 export default Album;
--- src/pages/Search.js
+++ src/pages/Search.js
@@ -1,11 +1,107 @@
 import React from 'react';
+// import searchAlbumsAPIs from '../services/searchAlbumsAPI';
 import Header from '../componentes/Header';
+import Loading from '../componentes/Loading';
+// import SearchAlbum from '../componentes/SearchAlbum';
 
 class Search extends React.Component {
+  constructor() {
+    super();
+
+    this.state = {
+      artist: '',
+      // nameArtist: '',
+      loading: false,
+      // albums: [],
+
+    };
+    this.handleChange = this.handleChange.bind(this);
+    this.buttonClick = this.buttonClick.bind(this);
+    // this.searchForm = this.searchForm.bind(this);
+    // this.discAlbums = this.discAlbums.bind(this);
+  }
+
+  handleChange({ target }) {
+    const { value } = target;
+    this.setState({
+      artist: value,
+    });
+  }
+
+  buttonClick(event) {
+    const { artist } = this.state;
+    event.preventDefault();
+    this.setState({
+      artist,
+      loading: true,
+    });
+    /* searchAlbumsAPIs(artist).then((artistAlbum) => {
+      this.setState({
+        loading: false,
+        albums: artistAlbum,
+        artist: '',
+      });
+    }); */
+  }
+
+  /* discAlbums() {
+    const { albums, nameArtist } = this.state;
+    return (
+      albums.length === 0 ? <p>Nenhum álbum foi encontrado</p>
+        : (
+          <>
+            <p>
+              Resultado de álbuns de:
+              {' '}
+              {`${nameArtist}`}
+            </p>
+
+            { albums.map((album) => (
+              <SearchAlbum
+                key={ album.collectionId }
+                album={ album }
+              />
+            )) }
+          </>
+        )
+    );
+  } */
+
+  searchForm() {
+    const { artist } = this.state;
+    const minLatter = 2;
+    return (
+      <form>
+        <label htmlFor="artista">
+          <input
+            type="text"
+            data-testid="search-artist-input"
+            value={ artist }
+            id="artista"
+            onChange={ this.handleChange }
+          />
+        </label>
+        <button
+          type="submit"
+          data-testid="search-artist-button"
+          disabled={ artist.length < minLatter }
+          onClick={ this.buttonClick }
+        >
+          Pesquisar
+
+        </button>
+      </form>
+    );
+  }
+
   render() {
+    const { loading } = this.state;
     return (
       <div data-testid="page-search">
         <Header />
+        <div>
+          <h2>{ loading ? <Loading /> : this.searchForm() }</h2>
+        </div>
       </div>
     );
   }
